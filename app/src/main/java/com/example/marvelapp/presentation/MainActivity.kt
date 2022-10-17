8ac56f04b416f8eeb0a1dc198be2b64a8d1d644a
Jonatas Jeremias Pena dos Santos <CIANDT\jonatasps@lnb035693cps.ciandt.global>
2022-10-17 20:03:23 -0300

    Implementando navigation

--- app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
@@ -2,11 +2,45 @@ package com.example.marvelapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.marvelapp.R
+import com.example.marvelapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {              //Ponto de entrada, pra gerenciar fluxo
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+                                        //Não é um fragmento comum, vai receber outros fragmentos para facilitar a navegação
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nave_host_container) as NavHostFragment
+
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
+            setOf()
+        )
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
